38e0487f094945a99c419700df7edaa7516c999e
OleksandraZakovorotna <x.boogieboo.x@gmail.com>
2026-01-17 10:18:10 +0200

    fix-task-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,9 +4,9 @@ const spanEl = document.querySelector("#name-output")
 inputEl.addEventListener("input", handleInput)
 
 function handleInput(event) {
-    const mesage = event.target.value;
+    const mesage = event.target.value.trim();
     
-    if (mesage.trim().length === 0){
+    if (mesage.length === 0){
         return spanEl.textContent = "Anonymous";
     } 
     else {
